43223ae147dce9105fdc26e957e855434677ede4
callmeyanzy <y4nn4n@gmail.com>
2026-02-14 11:42:38 +0800

    Add files via upload

---
+++ forcapstone/src/main/java/com/cscorner/forcapstone/ForcapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.cscorner.forcapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForcapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForcapstoneApplication.class, args);
+	}
+
+}
